d8b65f1792f35ae642e44256833d5ece7639cfc1
hyunsoo-ju <162404397+hyunsoo-ju@users.noreply.github.com>
2024-04-09 21:08:25 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i;
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a + b);
+    }
+
+    return 0;
+}
